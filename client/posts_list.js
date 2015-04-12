3102e6d60b769cd69d52c6ae7c2233802c0387ee
root <root@Meteor-dev.(none)>
2015-04-13 00:47:25 +0900

    commit

--- client/posts_list.js
+++ client/posts_list.js
@@ -1,17 +1,23 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://ecx.images-amazon.com/images/I/51tT2m9ZF0L._SL200_.jpg'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://ecx.images-amazon.com/images/I/51tT2m9ZF0L._SL200_.jpg'
-  },
-  {
-    title: 'The Meteor Book',
-    url: 'http://ecx.images-amazon.com/images/I/51tT2m9ZF0L._SL200_.jpg'
-  }
-];
 Template.testlist.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
+});
+
+Meteor.subscribe('posts');
+
+// counter starts at 0
+Session.setDefault('counter', 0);
+
+Template.hello.helpers({
+  counter: function () {
+    return Session.get('counter');
+  }
+});
+
+Template.hello.events({
+  'click button': function () {
+    // increment the counter when button is clicked
+    Session.set('counter', Session.get('counter') + 1);
+  }
 });
