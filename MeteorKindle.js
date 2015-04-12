315ded17759c24335b4632fd00f59f26c4aee9f6
gomesuit <gomesuit@gmail.com>
2015-04-12 20:28:30 +0900

    commit
    commit

--- MeteorKindle.js
+++ MeteorKindle.js
@@ -14,6 +14,14 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+  $(function(){
+    $('#container').masonry({
+      itemSelector: '.item',
+      columnWidth: 180,   
+      isFitWidth: true
+    });
+  });
 }
 
 if (Meteor.isServer) {
