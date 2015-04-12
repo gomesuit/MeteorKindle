8b4982bca228edc39d45a93a1dc38c77a8f2cc43
gomesuit <gomesuit@gmail.com>
2015-04-12 21:01:18 +0900

    commit
    commit

--- client/posts_list.js
+++ client/posts_list.js
@@ -1,9 +1,3 @@
-Template.test.helpers({
-  title: 'Introducing Telescope',
-  url: 'http://ecx.images-amazon.com/images/I/51tT2m9ZF0L._SL200_.jpg'
-});
-
-
 var postsData = [
   {
     title: 'Introducing Telescope',
