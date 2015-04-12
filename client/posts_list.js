cfb8d71e7de69df1e838f34a6186e8763ba76d21
gomesuit <gomesuit@gmail.com>
2015-04-12 20:59:05 +0900

    commit
    commit

--- client/posts_list.js
+++ client/posts_list.js
@@ -2,3 +2,22 @@ Template.test.helpers({
   title: 'Introducing Telescope',
   url: 'http://ecx.images-amazon.com/images/I/51tT2m9ZF0L._SL200_.jpg'
 });
+
+
+var postsData = [
+  {
+    title: 'Introducing Telescope',
+    url: 'http://ecx.images-amazon.com/images/I/51tT2m9ZF0L._SL200_.jpg'
+  },
+  {
+    title: 'Meteor',
+    url: 'http://ecx.images-amazon.com/images/I/51tT2m9ZF0L._SL200_.jpg'
+  },
+  {
+    title: 'The Meteor Book',
+    url: 'http://ecx.images-amazon.com/images/I/51tT2m9ZF0L._SL200_.jpg'
+  }
+];
+Template.testlist.helpers({
+  posts: postsData
+});
